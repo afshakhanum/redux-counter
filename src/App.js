ce1f191236cfa7f2cb64c33262c2f568214df66b
afsha khan <afshakhanstar@gmail.com>
2022-06-14 16:16:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css"
+import { useSelector, useDispatch } from 'react-redux'
+import { incNumber, decNumber } from './redux/action'
 
-function App() {
+const App = () => {
+  const myState = useSelector((state) => state.changeCount);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='App-header'>Counter</h1>
+      <div className='App'>
+      <h1>{myState}</h1>
+      <br></br>
+      <button onClick={ () => dispatch(decNumber())} className='App-button'>-</button>
+      <button onClick={ () => dispatch(incNumber())} className='App-button'>+</button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/redux/rootReducer.js
@@ -0,0 +1,8 @@
+import  changeCount from "./reducer";
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+    changeCount
+});
+
+export default rootReducer;
